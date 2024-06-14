731f3e1bf705b7417bf38acb6ed8b343d4c70542
Vahid Ramezani <vahid.ramezani.2014@gmail.com>
2024-06-14 12:36:39 +0330

    added solution

---
+++ src/main/java/Solution.java
@@ -0,0 +1,29 @@
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.PriorityQueue;
+
+class Solution {
+    public int findMaximizedCapital(int k, int w, int[] profits, int[] capital) {
+        int n = profits.length;
+        int[][] projects = new int[n][2];
+        for (int i = 0; i < n; i++) {
+            projects[i][0] = capital[i];
+            projects[i][1] = profits[i];
+        }
+        Arrays.sort(projects, Comparator.comparingInt(a -> a[0]));
+        int i = 0;
+        PriorityQueue<Integer> maximizeCapital = new PriorityQueue<>(Collections.reverseOrder());
+        while (k-- > 0) {
+            while (i < n && projects[i][0] <= w) {
+                maximizeCapital.offer(projects[i][1]);
+                i++;
+            }
+            if (maximizeCapital.isEmpty()) {
+                break;
+            }
+            w += maximizeCapital.poll();
+        }
+        return w;
+    }
+}
