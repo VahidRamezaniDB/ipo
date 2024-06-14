b920eddc11b3b5f2758c379b2d391e6fbc89959b
Vahid Ramezani <vahid.ramezani.2014@gmail.com>
2024-06-14 14:09:12 +0330

    testing

--- src/main/java/CaseGenerator.java
+++ src/main/java/CaseGenerator.java
@@ -1,3 +1,7 @@
+import java.util.Arrays;
+import java.util.Random;
+import java.util.stream.Collectors;
+
 public class CaseGenerator {
     private final Solution solution;
 
@@ -6,6 +10,28 @@ public class CaseGenerator {
     }
 
     public void generate(String inputFileName, String outputFileName) {
+        generateInputFile(inputFileName);
+    }
 
+    private void generateInputFile(String inputFileName) {
+        Random random = new Random(System.currentTimeMillis());
+        int size = random.nextInt(4, 10);
+        int k, w;
+        k = random.nextInt(size / 4, size);
+        int[] profits, capital;
+        profits = new int[size];
+        capital = new int[size];
+        for (int i = 0; i < size; i++) {
+            capital[i] = random.nextInt(10);
+            profits[i] = random.nextInt(10);
+        }
+        w = random.nextInt(10);
+        int result = solution.findMaximizedCapital(k, w, profits, capital);
+        System.out.println("size = " + size);
+        System.out.println("k = " + k);
+        System.out.println("w = " + w);
+        System.out.println("profits: " + Arrays.stream(profits).boxed().map(String::valueOf).collect(Collectors.joining(", ")));
+        System.out.println("capital: " + Arrays.stream(capital).boxed().map(String::valueOf).collect(Collectors.joining(", ")));
+        System.out.println("result: " + result);
     }
 }
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,10 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        int w = 0;
-        int k = 2;
-        int[] profits = {1,2,3};
-        int[] capital = {0,1,1};
-        Solution solution = new Solution();
-        System.out.println(solution.findMaximizedCapital(k,w,profits,capital));
+        CaseGenerator caseGenerator = new CaseGenerator(new Solution());
+        caseGenerator.generate("", "");
     }
 }
