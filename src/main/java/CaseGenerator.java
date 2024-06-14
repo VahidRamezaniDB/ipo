d6efdd3d2e8a0c47f3f857a5b0f65fa8344f4b00
Vahid Ramezani <vahid.ramezani.2014@gmail.com>
2024-06-14 12:39:55 +0330

    initial case generator

---
+++ src/main/java/CaseGenerator.java
@@ -0,0 +1,11 @@
+public class CaseGenerator {
+    private final Solution solution;
+
+    public CaseGenerator(Solution solution) {
+        this.solution = solution;
+    }
+
+    public void generate(String inputFileName, String outputFileName) {
+
+    }
+}
