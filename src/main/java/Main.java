ad4f97cc3ae78625773b6f281eefdf26455aaf4c
Vahid Ramezani <vahid.ramezani.2014@gmail.com>
2024-06-15 17:32:33 +0330

    modifications

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,10 @@
 public class Main {
     public static void main(String[] args) {
         CaseGenerator caseGenerator = new CaseGenerator(new Solution());
-        caseGenerator.generate("input1.txt", "output1.txt", 1, 10, 0, 100);
+        int index = 10;
+        String inputFileName = "input" + index + ".txt";
+        String outputFileName = "output" + index + ".txt";
+        int minSize = 90, maxSize = 100, minvalue = 90_000, maxValue = 100_000;
+        caseGenerator.generate(inputFileName, outputFileName, minSize, maxSize, minvalue, maxValue);
     }
 }
